28f4322a8cabe5ebcd9552ddb9c91de3d1925182
Max Streitberger <max.streitberger@gmail.com>
2022-03-15 10:08:32 +0100

    Create string helper file (string_helper.hpp)
    Move trim() function from ranker.hpp into string_helper.hpp

---
+++ include/helper/string_helper.hpp
@@ -0,0 +1,26 @@
+#ifndef STRING_HELPER_HPP
+#define STRING_HELPER_HPP
+
+#include <string>
+#include <algorithm>
+
+namespace stringhelper {
+    static inline void ltrim(std::string &s) {
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+            return !std::isspace(ch);
+        }));
+    }
+
+    static inline void rtrim(std::string &s) {
+        s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+            return !std::isspace(ch);
+        }).base(), s.end());
+    }
+
+    static inline void trim(std::string &s) {
+        ltrim(s);
+        rtrim(s);
+    }  
+} 
+
+#endif
--- modules/ranking/include/ranker.hpp
+++ modules/ranking/include/ranker.hpp
@@ -9,25 +9,7 @@
 
 #include "../../../include/doc_meta.hpp"
 #include "../../../include/token_meta.hpp"
-
-namespace stringhelper {
-    static inline void ltrim(std::string &s) {
-        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-            return !std::isspace(ch);
-        }));
-    }
-
-    static inline void rtrim(std::string &s) {
-        s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-            return !std::isspace(ch);
-        }).base(), s.end());
-    }
-
-    static inline void trim(std::string &s) {
-        ltrim(s);
-        rtrim(s);
-    }  
-} 
+#include "../../../include/helper/string_helper.hpp"
 
 struct Ranker {
     Ranker() {};
