af838beb5c4f1dbd074cd22840595a5393819fd1
Max Streitberger <max.streitberger@gmail.com>
2022-03-17 08:40:37 +0100

    Uncomment tests

--- modules/crawling/testing/src/crawler-test-cases.cpp
+++ modules/crawling/testing/src/crawler-test-cases.cpp
@@ -8,38 +8,38 @@ TEST_CASE("Crawler can", "[crawler][store]") {
     file << "Hello, World!";
     file.close();
 
-    // SECTION("retrieve all documents paths in a given folder.") {
-    //     std::vector<std::string> expectedDocuments = { 
-    //         CRAWLER_TESTING_ROOT_DIR "/testing-documents/demo.txt", 
-    //         CRAWLER_TESTING_ROOT_DIR "/testing-documents/demo2.txt", 
-    //         CRAWLER_TESTING_ROOT_DIR "/testing-documents/test-folder/demo3.txt" 
-    //     };
+    SECTION("retrieve all documents paths in a given folder.") {
+        std::vector<std::string> expectedDocuments = { 
+            CRAWLER_TESTING_ROOT_DIR "/testing-documents/demo.txt", 
+            CRAWLER_TESTING_ROOT_DIR "/testing-documents/demo2.txt", 
+            CRAWLER_TESTING_ROOT_DIR "/testing-documents/test-folder/demo3.txt" 
+        };
 
-    //     Crawler crawler = Crawler(CRAWLER_TESTING_ROOT_DIR "/testing-documents");
+        Crawler crawler = Crawler(CRAWLER_TESTING_ROOT_DIR "/testing-documents");
 
-    //     std::vector<std::string> visitedDocuments = crawler.getDocumentPaths();
+        std::vector<std::string> visitedDocuments = crawler.getDocumentPaths();
 
-    //     REQUIRE( expectedDocuments == visitedDocuments );
-    // }
+        REQUIRE( expectedDocuments == visitedDocuments );
+    }
 
-    // SECTION("retrieve the contents of a given document") {
-    //     std::string expectedContents = "Hello, World!";
+    SECTION("retrieve the contents of a given document") {
+        std::string expectedContents = "Hello, World!";
 
-    //     Crawler crawler = Crawler();
+        Crawler crawler = Crawler();
 
-    //     std::string visitedContents = crawler.getDocumentContents(CRAWLER_TESTING_ROOT_DIR "/testing-documents/demo.txt");
+        std::string visitedContents = crawler.getDocumentContents(CRAWLER_TESTING_ROOT_DIR "/testing-documents/demo.txt");
 
-    //     REQUIRE( expectedContents == visitedContents );
-    // }
+        REQUIRE( expectedContents == visitedContents );
+    }
 
-    // SECTION("register a new document") {
-    //     std::set<docmeta::DocumentMeta> expectedMeta = { docmeta::DocumentMeta(1, "Hello, World!", CRAWLER_TESTING_ROOT_DIR "/testing-documents/demo.txt") };
+    SECTION("register a new document") {
+        std::set<docmeta::DocumentMeta> expectedMeta = { docmeta::DocumentMeta(1, "Hello, World!", CRAWLER_TESTING_ROOT_DIR "/testing-documents/demo.txt") };
 
-    //     Crawler crawler = Crawler();
-    //     crawler.registerDocument(CRAWLER_TESTING_ROOT_DIR "/testing-documents/demo.txt");
+        Crawler crawler = Crawler();
+        crawler.registerDocument(CRAWLER_TESTING_ROOT_DIR "/testing-documents/demo.txt");
 
-    //     REQUIRE( expectedMeta == crawler.documents );
-    // }
+        REQUIRE( expectedMeta == crawler.documents );
+    }
 
     SECTION("identify and update an already registered document") {
         std::set<docmeta::DocumentMeta> expectedMeta = { docmeta::DocumentMeta(1, "Hello, World!", CRAWLER_TESTING_ROOT_DIR "/testing-documents/demo.txt") };
@@ -58,19 +58,19 @@ TEST_CASE("Crawler can", "[crawler][store]") {
         REQUIRE (crawler.documents.begin()->content == "What's up?" );
     }
 
-    // SECTION("push found documents to the store") {
-    //     std::set<docmeta::DocumentMeta> documentsToPush = { docmeta::DocumentMeta(1, "Hello, World!", CRAWLER_TESTING_ROOT_DIR "/testing-documents/demo.txt") };
+    SECTION("push found documents to the store") {
+        std::set<docmeta::DocumentMeta> documentsToPush = { docmeta::DocumentMeta(1, "Hello, World!", CRAWLER_TESTING_ROOT_DIR "/testing-documents/demo.txt") };
 
-    //     nlohmann::json expectedOutput = documentsToPush;
+        nlohmann::json expectedOutput = documentsToPush;
 
-    //     Crawler crawler = Crawler();
-    //     crawler.pushDocumentsToStore(documentsToPush);
+        Crawler crawler = Crawler();
+        crawler.pushDocumentsToStore(documentsToPush);
 
-    //     std::ifstream file("crawler-found-docs.json");
-    //     std::string resultOutput((std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
-    //     file.close();
+        std::ifstream file("crawler-found-docs.json");
+        std::string resultOutput((std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
+        file.close();
 
 
-    //     REQUIRE( expectedOutput.dump(4) == resultOutput );
-    // }
+        REQUIRE( expectedOutput.dump(4) == resultOutput );
+    }
 }
