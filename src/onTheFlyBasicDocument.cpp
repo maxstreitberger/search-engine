8d837a0a1273c1e6091753124f4a37e99cd19026
Max Streitberger <max.streitberger@gmail.com>
2022-03-28 10:19:57 +0200

    Add CLI search argument
    Currently, only the search flag is active. Later it will be possible to specify a path to the stopwords and the special character list.

--- src/onTheFlyBasicDocument.cpp
+++ src/onTheFlyBasicDocument.cpp
@@ -8,7 +8,30 @@
 #include "ranker.hpp"
 #include <nlohmann/json.hpp>
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        std::cout << "Error: no flags" << std::endl;
+        return 1;
+    }
+
+    std::string searchTerm;
+
+    for (int i = 1; i <= argc - 1; i++) {
+        std::string value = argv[i];
+
+        auto it = value.find('=');
+        if (it != std::string::npos) {
+            if ((value.find("search") != std::string::npos) || (value.find("-s=") != std::string::npos)) {
+                searchTerm = value.substr(it + 1, std::string::npos);
+            }
+        } else {
+            if ((value.find("search") != std::string::npos) || (value == "-s")) {
+                searchTerm = argv[i+1];
+            }
+        }
+    }
+
+    std::cout << "#### SEARCH FOR: " << searchTerm << " ####" << std::endl;
     std::set<docmeta::DocumentMeta> document_store;
     std::vector<docmeta::DocumentMeta> repository;
     std::map<std::string, std::set<tokenmeta::TokenMeta>> index;
@@ -23,7 +46,7 @@ int main() {
     indexer.generateIndex();
     
     Ranker ranking = Ranker(&document_store, &index);
-    std::vector<docmeta::DocumentMeta> foundDocuments = ranking.searchFor("Lorem");
+    std::vector<docmeta::DocumentMeta> foundDocuments = ranking.searchFor(searchTerm);
 
     for (auto& doc: foundDocuments) {
         std::cout << doc << std::endl;
--- src/onTheFlyBasicWeb.cpp
+++ src/onTheFlyBasicWeb.cpp
@@ -6,7 +6,31 @@
 #include "indexer.hpp"
 #include "ranker.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        std::cout << "Error: no flags" << std::endl;
+        return 1;
+    }
+
+    std::string searchTerm;
+
+    for (int i = 1; i <= argc - 1; i++) {
+        std::string value = argv[i];
+
+        auto it = value.find('=');
+        if (it != std::string::npos) {
+            if ((value.find("search") != std::string::npos) || (value.find("-s=") != std::string::npos)) {
+                searchTerm = value.substr(it + 1, std::string::npos);
+            }
+        } else {
+            if ((value.find("search") != std::string::npos) || (value == "-s")) {
+                searchTerm = argv[i+1];
+            }
+        }
+    }
+
+    std::cout << "#### SEARCH FOR: " << searchTerm << " ####" << std::endl;
+
     std::set<docmeta::DocumentMeta> page_store;
     std::vector<docmeta::DocumentMeta> repository;
     std::map<std::string, std::set<tokenmeta::TokenMeta>> index;
@@ -21,7 +45,7 @@ int main() {
     indexer.generateIndex();
 
     Ranker ranking = Ranker(&page_store, &index);
-    std::vector<docmeta::DocumentMeta> foundDocuments = ranking.searchFor("Lorem");
+    std::vector<docmeta::DocumentMeta> foundDocuments = ranking.searchFor(searchTerm);
 
     for (auto& doc: foundDocuments) {
         std::cout << doc << std::endl;
