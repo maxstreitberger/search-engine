35d95abce7998ddfaac35c0296a47112cd525d69
Max Streitberger <max.streitberger@gmail.com>
2022-03-28 08:10:59 +0200

    Connect web crawler to indexer and ranker
    This simple implementation was only possible because I used the DocumentMeta struct instead of the PageMeta struct since the indexer and ranker work with the DocumentMeta struct.

--- src/onTheFlyBasicWeb.cpp
+++ src/onTheFlyBasicWeb.cpp
@@ -3,20 +3,28 @@
 
 #include <iostream>
 #include "crawler.hpp"
+#include "indexer.hpp"
+#include "ranker.hpp"
 
 int main() {
     std::set<docmeta::DocumentMeta> page_store;
     std::vector<docmeta::DocumentMeta> repository;
+    std::map<std::string, std::set<tokenmeta::TokenMeta>> index;
+
+    std::string specialCharsPath = INDEXING_ROOT_DIR "/documents/special.txt";
+    std::string stopwordsPath = INDEXING_ROOT_DIR "/documents/stopwords.json";
 
     WebCrawler crawler = WebCrawler(&page_store, &repository, "https://zelebrate.xyz");
     crawler.start();
 
-    for (auto& page: repository) {
-        std::cout << page << std::endl;
-    }
+    Indexer indexer = Indexer(specialCharsPath, stopwordsPath, &repository, &index);
+    indexer.generateIndex();
 
-    for (auto& page: page_store) {
-        std::cout << page << std::endl;
+    Ranker ranking = Ranker(&page_store, &index);
+    std::vector<docmeta::DocumentMeta> foundDocuments = ranking.searchFor("Lorem");
+
+    for (auto& doc: foundDocuments) {
+        std::cout << doc << std::endl;
     }
 
     return 0;
