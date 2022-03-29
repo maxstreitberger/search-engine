17ce87c315f0eea8f51f0fa4c829fb3a164b37e0
Max Streitberger <max.streitberger@gmail.com>
2022-03-29 10:22:07 +0200

    Add testing coverage

--- test-script.cmake
+++ test-script.cmake
@@ -1,10 +1,12 @@
 set(CTEST_SOURCE_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}")
-set(CTEST_BINARY_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}/bin/ctest")
+set(CTEST_BINARY_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}/bin")
+set(CTEST_CMAKE_COMMAND "/usr/local/bin/cmake")
+set(CTEST_COMMAND "/usr/local/bin/ctest -D Experimental")
 
 ctest_empty_binary_directory("${CTEST_BINARY_DIRECTORY}")
 
-# set(CTEST_SITE "${CMAKE_SYSTEM_NAME}")
-# set(CTEST_BUILD_NAME "experimental build")
+set(CTEST_SITE "${CMAKE_SYSTEM_NAME}")
+set(CTEST_BUILD_NAME "experimental build")
 
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(CTEST_COVERAGE_COMMAND "gcov")
@@ -13,5 +15,5 @@ ctest_start("Experimental")
 ctest_configure()
 ctest_build()
 ctest_test()
-# ctest_coverage()
+ctest_coverage()
 ctest_submit()
