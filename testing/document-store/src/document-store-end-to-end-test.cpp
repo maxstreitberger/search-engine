2c87aec1f8f0f3f511b24ba14e049610e17c03e5
Max Streitberger <max.streitberger@gmail.com>
2022-04-11 08:27:06 +0200

    Add initial benchmarking to document-store

--- testing/document-store/src/document-store-end-to-end-test.cpp
+++ testing/document-store/src/document-store-end-to-end-test.cpp
@@ -1,4 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
 #include "doc_store.hpp"
 
 TEST_CASE("Document Store end-to-end test: Unique documents.", "[store][e2e]") {
@@ -25,6 +26,12 @@ TEST_CASE("Document Store end-to-end test: Unique documents.", "[store][e2e]") {
 
     REQUIRE( document_store == expected_store);
     REQUIRE( repository == expected_repository);
+
+    document_store = {};
+    repository = {};
+    BENCHMARK("Document Store end-to-end test: Unique documents.") {
+        return store.processDocuments();
+    };
 }
 
 TEST_CASE("Document Store end-to-end test: Updated document.", "[store][e2e]") {
@@ -50,6 +57,12 @@ TEST_CASE("Document Store end-to-end test: Updated document.", "[store][e2e]") {
 
     REQUIRE( document_store == expected_store);
     REQUIRE( repository == expected_repository);
+
+    document_store = {};
+    repository = {};
+    BENCHMARK("Document Store end-to-end test: Updated document.") {
+        return store.processDocuments();
+    };
 }
 
 TEST_CASE("Document Store end-to-end test: Nothing to change.", "[store][e2e]") {
@@ -71,5 +84,10 @@ TEST_CASE("Document Store end-to-end test: Nothing to change.", "[store][e2e]") 
 
     REQUIRE( document_store == expected_store);
     REQUIRE( repository.empty() );
+
+    repository = {};
+    BENCHMARK("Document Store end-to-end test: Nothing to change.") {
+        return store.processDocuments();
+    };
 }
 
--- testing/document-store/src/document-store-test-cases.cpp
+++ testing/document-store/src/document-store-test-cases.cpp
@@ -1,4 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
 #include "doc_store.hpp"
 
 std::set<docmeta::DocumentMeta> crawler_docs = {
@@ -17,6 +18,10 @@ TEST_CASE("Document Store can check for new documents.", "[store]") {
     std::vector<docmeta::DocumentMeta> foundNewDocuments = store.checkForChanges(&resultingStore, &crawler_docs).first;
 
     REQUIRE( expectedResult == foundNewDocuments );
+
+    BENCHMARK("Document Store can check for new documents.") {
+        return store.checkForChanges(&resultingStore, &crawler_docs).first;
+    };
 }
 
 TEST_CASE("Document Store can check for updated documents.", "[store]") {
@@ -29,6 +34,10 @@ TEST_CASE("Document Store can check for updated documents.", "[store]") {
     std::vector<docmeta::DocumentMeta> resultingUpdates = store.checkForChanges(&crawler_docs, &toUpdateDocument).second;
 
     REQUIRE( expectedResult == resultingUpdates );
+
+    BENCHMARK("Document Store can check for updated documents.") {
+        return store.checkForChanges(&crawler_docs, &toUpdateDocument).second;
+    };
 }
 
 TEST_CASE("Document Store can add new documments to the store.", "[store]") {
@@ -41,6 +50,11 @@ TEST_CASE("Document Store can add new documments to the store.", "[store]") {
     store.addNewDocumentsToStore(&resultingStore, newDocumment);
 
     REQUIRE( expectedStore == resultingStore );
+
+    resultingStore = {};
+    BENCHMARK("Document Store can check for updated documents.") {
+        return store.addNewDocumentsToStore(&resultingStore, newDocumment);
+    };
 }
 
 TEST_CASE("Document Store can update documments in the store.", "[store]") {
@@ -53,4 +67,9 @@ TEST_CASE("Document Store can update documments in the store.", "[store]") {
     store.updateDocumentsInStore(&resultingStore, updatedDocument);
 
     REQUIRE( expectedStore == resultingStore );
+
+    resultingStore = { docmeta::DocumentMeta(1, "Hello, World!", "Some Example Path #1") };
+    BENCHMARK("Document Store can update documments in the store.") {
+        return store.updateDocumentsInStore(&resultingStore, updatedDocument);
+    };
 }
