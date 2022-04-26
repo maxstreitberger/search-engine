309965696efb5bdb1eddb0415edfdcc79179b7c9
Max <max.streitberger@gmail.com>
2022-04-26 09:55:38 +0200

    Update helper tests

--- testing/helper/test-cases.cpp
+++ testing/helper/test-cases.cpp
@@ -1,35 +1,35 @@
 #include <catch2/catch_test_macros.hpp>
 #include "helpers.hpp"
 
-TEST_CASE("Can remove white space infront of a text", "[on-the-fly, basic, web, e2e]") {
+TEST_CASE("Can remove white space infront of a text.", "[helpers]") {
     std::string text = "  hello";
     std::string expectedText = "hello";
     helpers::ltrim(text);
     REQUIRE( text == expectedText );
 }
 
-TEST_CASE("Can remove white space behind a text", "[on-the-fly, basic, web, e2e]") {
+TEST_CASE("Can remove white space behind a text.", "[helpers]") {
     std::string text = "hello  ";
     std::string expectedText = "hello";
     helpers::rtrim(text);
     REQUIRE( text == expectedText );
 }
 
-TEST_CASE("Can remove white space around a text", "[on-the-fly, basic, web, e2e]") {
+TEST_CASE("Can remove white space around a text.", "[helpers]") {
     std::string text = "  hello  ";
     std::string expectedText = "hello";
     helpers::trim(text);
     REQUIRE( text == expectedText );
 }
 
-TEST_CASE("Can make a text to lowercase", "[on-the-fly, basic, web, e2e]") {
+TEST_CASE("Can make a text to lowercase.", "[helpers]") {
     std::string text = "HELLO";
     std::string expectedText = "hello";
     helpers::toLower(text);
     REQUIRE( text == expectedText );
 }
 
-TEST_CASE("Can load text", "[on-the-fly, basic, web, e2e]") {
+TEST_CASE("Can load text.", "[helpers]") {
     std::string filePath = SEARCHENGINE_TESTING_DIR "/resources/test-documents/doc1.txt";
     std::string returnedText = helpers::loadFile(filePath);
     REQUIRE( returnedText == "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum malesuada nisl, in posuere ligula aliquam et. Vivamus dapibus sapien a maximus suscipit. Maecenas id viverra mauris. Phasellus et lorem quis massa pretium elementum. Donec feugiat diam ut lorem pellentesque, eget eleifend elit iaculis. Mauris vitae libero pulvinar, egestas erat." );
